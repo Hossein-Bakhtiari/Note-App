6a2c0d8ca58301e539af3890da06e8207e7f9add
hossein <hosseinbakhtiari781@outlook.com>
2023-12-09 17:49:57 +0330

    useLocalStorageHook and Note creation Logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,24 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from "react-bootstrap"
 import { Route, Routes , Navigate } from "react-router-dom"
 import { NewNote } from "./NewNote"
+import { useLocalStorage } from "./useLocalStorage"
+import { useMemo } from "react"
+
 
 export type Note = {
   id: string
 } & NoteData
+
+export type RawNote = {
+  id: string
+
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
 
 export type NoteData = {
   title: string
@@ -19,6 +33,15 @@ export type Tag = {
 }
 
 function App() {
+  const [ notes , setNotes] = useLocalStorage<RawNote[]>("NOTES" , [])
+  const [ tags , setTags] = useLocalStorage<Tag[]>("TAGS" , [])
+
+  const noteWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {...note , tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes , tags])
+
   return (
     <Container className="my-4">
     <Routes>
