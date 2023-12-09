f02b729acbb6d879f55e03ee5990ec8e00ab3ded
hossein <hosseinbakhtiari781@outlook.com>
2023-12-09 16:34:57 +0330

    New note from JsX

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from "react-bootstrap"
 import { Route, Routes , Navigate } from "react-router-dom"
+import { NewNote } from "./NewNote"
 function App() {
   return (
-    <Container>
+    <Container className="my-4">
     <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/new" element={<NewNote />} />
         <Route path="/:id">
             <Route index element={<h1>Show</h1>} />
             <Route path="edit" element={<h1>Edit</h1>} />
---
+++ src/NewNote.tsx
@@ -0,0 +1,10 @@
+import { NoteForme } from "./NoteForme";
+
+export function NewNote(){
+    return (
+        <>
+        <h1 className="mb-4">New Note</h1>
+        <NoteForme />
+        </>
+    )
+}
