399b1884664f90adca2a1616b5bd6763975064b9
hossein <hosseinbakhtiari781@outlook.com>
2023-12-10 22:39:42 +0330

    finish tag creation logic

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,15 +1,17 @@
-import { NoteData } from "./App";
+import { NoteData, Tag } from "./App";
 import { NoteForme } from "./NoteForme";
 
 type NewNoteProps = {
     onSubmit: (data: NoteData) => void
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
 }
 
-export function NewNote({onSubmit}: NewNoteProps){
+export function NewNote({onSubmit , onAddTag ,availableTags}: NewNoteProps){
     return (
         <>
         <h1 className="mb-4">New Note</h1>
-        <NoteForme onSubmit={onSubmit}/>
+        <NoteForme onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags}/>
         </>
     )
 }
--- src/NoteForme.tsx
+++ src/NoteForme.tsx
@@ -5,11 +5,13 @@ import { v4 as uuidV4 } from "uuid"
 
 type NoteFormProps = {
     onSubmit: (data: NoteData) => void
+    onAddTag: (tag: Tag) => void
+    availableTags: Tag[]
 }
 
 import CreatableReactSelect from "react-select/creatable"
 import { NoteData, Tag } from "./App";
-export function NoteForme({ onSubmit }: NoteFormProps){
+export function NoteForme({ onSubmit , onAddTag , availableTags }: NoteFormProps){
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags , setSelectedTags] = useState<Tag[]>([])
@@ -41,9 +43,13 @@ export function NoteForme({ onSubmit }: NoteFormProps){
                         onCreateOption={label => {
                             const newTag = { id: uuidV4(), label  }
                             onAddTag(newTag)
+                            setSelectedTags(prev => [...prev , newTag])
                         }}
                         value={selectedTags.map(tag => {
                             return {label: tag.label , value: tag.id}
+                        })}
+                        options={availableTags.map(tag => {
+                            return { label: tag.label , value: tag.id }
                         })}
                         onChange={tags => {
                             setSelectedTags(
