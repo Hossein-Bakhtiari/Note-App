7da5dc8139e1820f935cd8041b8328f9b219aa40
hossein <hosseinbakhtiari781@outlook.com>
2023-12-11 19:38:15 +0330

    start creating Note List page

--- src/NoteForme.tsx
+++ src/NoteForme.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { v4 as uuidV4 } from "uuid"
 
 type NoteFormProps = {
@@ -15,14 +15,17 @@ export function NoteForme({ onSubmit , onAddTag , availableTags }: NoteFormProps
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags , setSelectedTags] = useState<Tag[]>([])
+    const navigate = useNavigate()
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault();
         onSubmit({
             title: titleRef.current!.value,
             markdown: markdownRef.current!.value,
-            tags: [],
+            tags: selectedTags,
         })
+
+        navigate("..")
     }
 
 
